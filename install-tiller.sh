334e37043f2297cf0b64ccc841bd48ad9b6e589f
Simon Massey <simon@simonmassey.org>
2019-01-01 20:17:01 +0000

    install-tiller.sh

---
+++ install-tiller.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+TILLER_NAMESPACE=$(oc project --short)
+
+read -p "Helm version? (default: v2.11.0): " HELM_VERSION
+[ -z "${HELM_VERSION}" ] && HELM_VERSION='v2.11.0'
+
+read -e -p "Install tiller into ${TILLER_NAMESPACE}? [Y/n] " YN
+
+if [[ $YN == "y" || $YN == "Y" || $YN == "" ]]; then
+  oc process \
+    -f https://raw.githubusercontent.com/simbo1905/origin/master/examples/helm/tiller-template.yaml \
+    -p TILLER_NAMESPACE="${TILLER_NAMESPACE}" \
+    -p HELM_VERSION="${HELM_VERSION}"  \
+  | oc create -f - 
+fi
